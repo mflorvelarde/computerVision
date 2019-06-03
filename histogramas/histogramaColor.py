e667e092fa8c92d3c77b5b11515410a539018e2d
Florencia <mflorencia.velarde@gmail.com>
2019-06-03 05:46:00 -0300

    histogramas

--- histogramas/histogramaColor.py
+++ histogramas/histogramaColor.py
@@ -1,15 +1,31 @@
+import cv2 as cv
 import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-import cv2
 
-img = cv2.imread('goat.jpeg')
-color = ('b','g','r')
-for i,col in enumerate(color):
-    histr = cv2.calcHist([img],[i],None,[256],[0,256])
-    plt.plot(histr,color = col)
-    plt.xlim([0,256])
+img = cv.imread('goat.jpeg',0)
+plt.hist(img.ravel(),256,[0,256])
 plt.show()
+
+
+cap = cv.VideoCapture(0)
+while(True):
+    ret, frame = cap.read()
+    color = ('b', 'g', 'r')
+    for i, col in enumerate(color):
+        histr = cv.calcHist([frame], [i], None, [256], [0, 256])
+        plt.plot(histr, color=col)
+        plt.xlim([0, 256])
+    plt.show()
+
+    k = cv.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv.destroyAllWindows()
+
+
 
 
--- histogramas/histogramaMonocromatico.py
+++ histogramas/histogramaMonocromatico.py
@@ -1,12 +1,22 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-import cv2
 
-img = cv2.imread('goat.jpeg',0)
+img = cv.imread('goat.jpeg',0)
 plt.hist(img.ravel(),256,[0,256])
 plt.show()
 
 
+cap = cv.VideoCapture(0)
+while(True):
+    ret, frame = cap.read()
+    plt.hist(frame.ravel(), 256, [0, 256])
+    plt.show()
+    k = cv.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv.destroyAllWindows()
