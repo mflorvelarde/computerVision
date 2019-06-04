353763775afdbe8ea7d8009152e4bcb16c668570
Florencia <mflorencia.velarde@gmail.com>
2019-06-03 21:44:19 -0300

    Linear blend with camera

--- otrasTransformaciones/linearBlend.py
+++ otrasTransformaciones/linearBlend.py
@@ -1,18 +1,24 @@
 import cv2
 
-src1 = cv2.imread('cielo.jpeg')
-src2 = cv2.imread('goat.jpeg')
+cielo = cv2.imread('cielo.jpeg')
 
 alpha = 0.8
 beta = (1.0 - alpha)
-resize = cv2.resize(src1,(300,168))
-cv2.imshow('goat', src2)
-cv2.imshow('cielo', resize)
-
-dst = cv2.addWeighted(resize, alpha, src2, beta, 0.0)
+cieloResize = cv2.resize(cielo,(400,300))
 
 
-cv2.imshow('Linear blend', dst)
-cv2.waitKey(0)
+cap = cv2.VideoCapture(0)
 
+while(True):
+    ret, frame = cap.read()
+    frameResize = cv2.resize(frame, (400,300))
+    dst = cv2.addWeighted(cieloResize, alpha, frameResize, beta, 0.0)
+
+    cv2.imshow('Linear blend', dst)
+
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
 cv2.destroyAllWindows()
